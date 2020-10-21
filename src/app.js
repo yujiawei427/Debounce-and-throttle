5c72f7c5045ab7cb3cdc82931fa01e4195c38a13
yujiawei427 <yujiawei427@gmail.com>
2020-10-22 02:30:57 +1100

    CI/CD

--- src/app.js
+++ src/app.js
@@ -5,5 +5,5 @@ require('dotenv').config(); //api加密
 
 app.use(router);
 app.listen(process.env.PORT, ()=> {
-  console.log('Server is running on 2001');
+  console.log(`Server is running on ${process.env.PORT}`);
 });
